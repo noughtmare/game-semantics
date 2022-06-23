7e3cef16f96e86ecbf7dc0dda80ba1917b02e18e
Jacco Krijnen <j.o.g.krijnen@uu.nl>
2022-06-22 21:16:38 -0700

    Make Arenas a type-class

--- Game.hs
+++ Game.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 module Game where
 
 import Data.Set (Set)
@@ -5,44 +7,41 @@ import qualified Data.Set as Set
 
 import Common
 
-
-data Arena a = MkArena
-  { enabling :: a -> a -> Bool
-  , initial  :: Set a
-  }
+data Ty = TyNat
 
 -- | Point to questions asked
 type Pointer = Nat
 
-data Turn q v = Turn (Move q v) (Move q v) Pointer
+data Move ty = Q (Question ty) | Val (Value ty)
+data Turn (ty :: Ty) = Turn (Move ty) (Move ty) Pointer
+type Play (ty :: Ty) = [Turn ty]
 
-type Play q v = [Turn q v]
-
-isLegal :: Play q v -> Bool
+isLegal :: Play ty -> Bool
 isLegal = go 0
   where
     -- n is the amount of questions of opponent
     go n [] = True
     go n (Turn o _ ref : ts) = ref < n && go (if isQ o then S n else n) ts
 
-
-data Lang
-  = Nat Nat
-
-
-data Move q v = Q q | Val v
-
-isQ :: Move q v -> Bool
+isQ :: Move ty -> Bool
 isQ (Q _) = True
 isQ _     = False
 
-data Polarity = P | O
+
+class Arena (ty :: Ty) where
+
+  type family Question ty :: *
+  type family Value ty :: *
+
+  enabling :: Move ty -> Move ty -> Bool
+  initial  :: Set (Move ty)
 
 
--- | There is only one question (), values are Nats
-type NatMove = Move () Nat
+instance Arena TyNat where
+  type Question TyNat = ()
+  type Value TyNat = Nat
 
-natArena :: Arena NatMove
-natArena = MkArena natEnabling (Set.singleton (Q ())) where
-  natEnabling (Q _) (Val _) = True
-  natEnabling _ _ = False
+  enabling (Q _) (Val _) = True
+  enabling _ _ = False
+
+  initial = Set.singleton (Q ())
