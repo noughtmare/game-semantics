1cb89d7a0bece967da7e51abfc7c8980fc4d7f6e
Jacco Krijnen <j.o.g.krijnen@uu.nl>
2022-06-22 19:01:09 -0700

    Add initial moves, Nat, Polarity

--- Game.hs
+++ Game.hs
@@ -1,13 +1,29 @@
 module Game where
 
+import Data.Set (Set)
+import qualified Data.Set as Set
 
-data Arena a = MkArena { enabling :: a -> a -> Bool }
+
+data Arena a = MkArena
+  { enabling :: a -> a -> Bool
+  , initial :: Set a
+  }
 
 type Play a = [a]
 
-data NatMove = Q | N Int
+data Nat = Z | S Nat
+
+instance Num Nat where
+  fromInteger 0 = Z
+  fromInteger n
+    | n > 0 = S (fromIntegral n)
+    | otherwise = error "boom"
+
+data NatMove = Q | N Nat
+
+data Polarity = P | O
 
 natArena :: Arena NatMove
-natArena = MkArena natEnabling where
+natArena = MkArena natEnabling (Set.singleton Q) where
   natEnabling Q (N _) = True
   natEnabling _ _ = False
